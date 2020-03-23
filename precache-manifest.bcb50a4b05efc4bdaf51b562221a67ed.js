db54bd1540807cc38a52e1ca102c54216970bc4b
jtbspace <jtbspace.tech@gmail.comm>
2020-03-23 18:43:59 +1300

    Updates

---
+++ precache-manifest.bcb50a4b05efc4bdaf51b562221a67ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6ac7e7ec76372fe137a236e1e0e8a94",
+    "url": "/my_youtube/index.html"
+  },
+  {
+    "revision": "1ed808e2ffad0c8f6d75",
+    "url": "/my_youtube/static/css/main.9ece4117.chunk.css"
+  },
+  {
+    "revision": "e97302a1e484c50b9a19",
+    "url": "/my_youtube/static/js/2.3c6813fa.chunk.js"
+  },
+  {
+    "revision": "6b1c9aa1f24077e4bfd4b51db2988c14",
+    "url": "/my_youtube/static/js/2.3c6813fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ed808e2ffad0c8f6d75",
+    "url": "/my_youtube/static/js/main.e64152bc.chunk.js"
+  },
+  {
+    "revision": "80a061f6559727285f8b",
+    "url": "/my_youtube/static/js/runtime-main.f9820224.js"
+  }
+]);
